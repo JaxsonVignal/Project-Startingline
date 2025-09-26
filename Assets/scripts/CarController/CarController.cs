ddcb39ccceaed5ea514f6c708a6f1b9a30cbe5d7
Clanka-Clanker <asaduqailipro321@gmail.com>
2025-09-26 18:57:05 -0400

    Car movement Update

--- Assets/scripts/CarController/CarController.cs
+++ Assets/scripts/CarController/CarController.cs
@@ -18,15 +18,20 @@ public class CarController : MonoBehaviour
         public GameObject wheelModel;
         public WheelCollider wheelCollider;
         public Axel axel;
-       
     }
 
     public float maxAcceleration = 30.0f;
     public float brakeAcceleration = 50.0f;
 
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public Vector3 _centerOfMass;
+
     public List<Wheel> wheels;
 
     float moveInput;
+    float steerInput;
 
     private Rigidbody carRB;
 
@@ -34,6 +39,7 @@ public class CarController : MonoBehaviour
     private void Start()
     {
         carRB = GetComponent<Rigidbody>();
+        carRB.centerOfMass = _centerOfMass;
     }
 
     void Update()
@@ -41,15 +47,16 @@ public class CarController : MonoBehaviour
         GetInputs();
     }
 
-    void Lateupdate()
+    void LateUpdate()
     {
         Move();
-        
+        Steer();
     }
 
     void GetInputs()
     {
         moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
     }
 
     
@@ -59,9 +66,31 @@ public class CarController : MonoBehaviour
      {
       wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
      }    
-    
-    
     }
+
+    void Steer() 
+    { 
+      foreach(var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+               var _steerAngle = steerInput * turnSensitivity; * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
