a0874132b2cefce56cada7f7f5f77774f69d0ca4
Jaxson Vignal <58239948+JaxsonVignal@users.noreply.github.com>
2025-10-15 12:01:07 -0700

    attachments added to save load system

--- Assets/scripts/SaveSystem/SaveData.cs
+++ Assets/scripts/SaveSystem/SaveData.cs
@@ -2,13 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class WeaponAttachmentSaveData
+{
+    public string attachmentId;
+    public AttachmentType type;
+    public float posX, posY, posZ;
+    public float rotX, rotY, rotZ;
+    public float scaleX, scaleY, scaleZ;
+
+    public WeaponAttachmentSaveData(WeaponAttachmentEntry entry)
+    {
+        attachmentId = entry.attachmentId;
+        type = entry.type;
+        posX = entry.posX;
+        posY = entry.posY;
+        posZ = entry.posZ;
+        rotX = entry.rotX;
+        rotY = entry.rotY;
+        rotZ = entry.rotZ;
+        scaleX = entry.scaleX;
+        scaleY = entry.scaleY;
+        scaleZ = entry.scaleZ;
+    }
+
+    public WeaponAttachmentEntry ToEntry()
+    {
+        return new WeaponAttachmentEntry(
+            attachmentId,
+            type,
+            new Vector3(posX, posY, posZ),
+            new Vector3(rotX, rotY, rotZ),
+            new Vector3(scaleX, scaleY, scaleZ)
+        );
+    }
+}
+
+[System.Serializable]
+public class WeaponInstanceSaveData
+{
+    public string weaponId;
+    public string instanceId;
+    public string displayName;
+    public List<WeaponAttachmentSaveData> attachments;
+    public int currentAmmo;
+
+    public WeaponInstanceSaveData(WeaponInstance instance)
+    {
+        weaponId = instance.weaponId;
+        instanceId = instance.instanceId;
+        displayName = instance.displayName;
+        currentAmmo = instance.currentAmmo;
+
+        attachments = new List<WeaponAttachmentSaveData>();
+        foreach (var att in instance.attachments)
+        {
+            attachments.Add(new WeaponAttachmentSaveData(att));
+        }
+    }
+
+    public WeaponInstance ToInstance()
+    {
+        var instance = new WeaponInstance
+        {
+            weaponId = weaponId,
+            instanceId = instanceId,
+            displayName = displayName,
+            currentAmmo = currentAmmo
+        };
+
+        foreach (var attData in attachments)
+        {
+            instance.attachments.Add(attData.ToEntry());
+        }
+
+        return instance;
+    }
+}
+
 public class SaveData
 {
     public List<string> collectedItems;
     public SerializableDictionary<string, InventorySaveData> ChestDictionary;
     public SerializableDictionary<string, ItemPickUpSaveData> activeItems;
     public InventorySaveData playerInventory;
-    public SerializableDictionary<string, int> weaponAmmoData; // NEW: Store weapon ammo
+    public SerializableDictionary<string, int> weaponAmmoData;
+
+    // NEW: Store weapon instances with all attachment data per slot
+    public SerializableDictionary<string, WeaponInstanceSaveData> weaponInstances;
 
     public SaveData()
     {
@@ -16,6 +97,7 @@ public class SaveData
         ChestDictionary = new SerializableDictionary<string, InventorySaveData>();
         activeItems = new SerializableDictionary<string, ItemPickUpSaveData>();
         playerInventory = new InventorySaveData();
-        weaponAmmoData = new SerializableDictionary<string, int>(); // NEW
+        weaponAmmoData = new SerializableDictionary<string, int>();
+        weaponInstances = new SerializableDictionary<string, WeaponInstanceSaveData>(); // NEW
     }
 }
--- Assets/scripts/SaveSystem/SaveLoad.cs
+++ Assets/scripts/SaveSystem/SaveLoad.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Events;
-public static class SaveLoad 
+
+public static class SaveLoad
 {
     public static UnityAction OnSaveGame;
     public static UnityAction<SaveData> OnLoadGame;
-
     private static string directory = "/SaveData/";
     private static string fileName = "SaveGame.sav";
 
@@ -16,49 +16,88 @@ public static class SaveLoad
     {
         OnSaveGame?.Invoke();
 
+        // NEW: Save all weapon instances with attachments before saving data
+        SaveWeaponInstances(data);
+
         string dir = Application.persistentDataPath + directory;
-
         GUIUtility.systemCopyBuffer = dir;
-
-        if(!Directory.Exists(dir))
+        if (!Directory.Exists(dir))
         {
             Directory.CreateDirectory(dir);
         }
-
         string json = JsonUtility.ToJson(data, prettyPrint: true);
         File.WriteAllText(path: dir + fileName, contents: json);
-
-        Debug.Log(message: "saving game");
+        Debug.Log(message: "Saving game with weapon instances");
         return true;
     }
 
     public static SaveData Load()
     {
         string fullPath = Application.persistentDataPath + directory + fileName;
-
         SaveData data = new SaveData();
-
-        if(File.Exists(fullPath))
+        if (File.Exists(fullPath))
         {
             string json = File.ReadAllText(fullPath);
             data = JsonUtility.FromJson<SaveData>(json);
 
+            // NEW: Load all weapon instances with attachments
+            LoadWeaponInstances(data);
+
             OnLoadGame?.Invoke(data);
+            Debug.Log("Game loaded with weapon instances restored");
         }
         else
         {
             Debug.Log("Savefile does not exist");
         }
         return data;
-    } 
+    }
 
     public static void DeleteSaveData()
     {
         string fullPath = Application.persistentDataPath + directory + fileName;
-
-        if(File.Exists(fullPath))
+        if (File.Exists(fullPath))
         {
             File.Delete(fullPath);
+            WeaponInstanceStorage.ClearAll();
+            Debug.Log("Save data and weapon instances deleted");
+        }
+    }
+
+    // NEW: Save all weapon instances to SaveData
+    private static void SaveWeaponInstances(SaveData data)
+    {
+        data.weaponInstances.Clear();
+
+        // Get all inventory slots and save their weapon instances
+        var playerInventory = GameObject.FindObjectOfType<PlayerInventoryHolder>();
+        if (playerInventory != null)
+        {
+            foreach (var slot in playerInventory.PrimaryInventorySystem.InventorySlots)
+            {
+                if (slot.ItemData is WeaponData)
+                {
+                    var instance = WeaponInstanceStorage.GetInstance(slot.UniqueSlotID);
+                    if (instance != null)
+                    {
+                        data.weaponInstances[slot.UniqueSlotID] = new WeaponInstanceSaveData(instance);
+                        Debug.Log($"Saved weapon instance for slot {slot.UniqueSlotID} with {instance.attachments.Count} attachments");
+                    }
+                }
+            }
+        }
+    }
+
+    // NEW: Load all weapon instances from SaveData
+    private static void LoadWeaponInstances(SaveData data)
+    {
+        WeaponInstanceStorage.ClearAll();
+
+        foreach (var kvp in data.weaponInstances)
+        {
+            var instance = kvp.Value.ToInstance();
+            WeaponInstanceStorage.StoreInstance(kvp.Key, instance);
+            Debug.Log($"Loaded weapon instance for slot {kvp.Key} with {instance.attachments.Count} attachments");
         }
     }
 }
